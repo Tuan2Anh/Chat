6a1770897570d21166446cb4f2e0e7b710b97d5c
Nguyễn Tuấn Anh <157350887+Tuan2Anh@users.noreply.github.com>
2025-01-14 13:56:45 +0000

    q

--- backend/auth-service/server.js
+++ backend/auth-service/server.js
@@ -1,13 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
+import cors from "cors";
 
 import authRoutes from "./auth.routes.js";
 import connectToMongoDB from "./connectToMongoDB.js";
-import cors from "cors";
-import { app, server } from "../socket/socket.js";
 
 dotenv.config();
+
+const app = express();
 const PORT = process.env.AUTH_PORT || 4000; // Separate port for auth server
 
 app.use(express.json()); // Parse incoming JSON payloads
@@ -15,12 +17,18 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
-server.listen(PORT, () => {
-    connectToMongoDB();
-    console.log(`Main Server Running on port ${PORT}`);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
 
 
+app.listen(PORT, () => {
+    connectToMongoDB();
+    console.log(`Auth Server Running on port ${PORT}`);
+});
 
 app.use(
     cors({
