0caacda1300b2ed637b9c4cf31490efaef256c2f
Nguyễn Tuấn Anh <157350887+Tuan2Anh@users.noreply.github.com>
2025-01-14 13:59:30 +0000

    r

--- backend/auth-service/server.js
+++ backend/auth-service/server.js
@@ -17,14 +17,6 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
-
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-});
-
-
 app.listen(PORT, () => {
     connectToMongoDB();
     console.log(`Auth Server Running on port ${PORT}`);
