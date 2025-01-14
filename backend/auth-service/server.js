4296ee9c5121f2d3457b6c8827ad8e9e585b01c5
Nguyễn Tuấn Anh <157350887+Tuan2Anh@users.noreply.github.com>
2025-01-14 12:52:20 +0000

    ee

--- backend/auth-service/server.js
+++ backend/auth-service/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 import authRoutes from "./routes/auth.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
@@ -14,6 +15,14 @@ app.use(express.json()); // Parse incoming JSON payloads
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
 
 app.listen(PORT, () => {
     connectToMongoDB();
